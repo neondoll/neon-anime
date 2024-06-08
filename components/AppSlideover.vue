71a2a2ebfdd53581814abb59e259d31d28944c47
neondoll <bonni_1998@mail.ru>
2024-06-08 18:45:06 +0300

    AppSlideover setup

--- components/AppSlideover.vue
+++ components/AppSlideover.vue
@@ -9,12 +9,7 @@ const { authenticated } = storeToRefs(useAuthStore());
 const { signOut } = useAuthStore();
 
 const links = computed<VerticalNavigationLink[]>(() => {
-  return [
-    ...(user.value !== null ? [{ label: user.value?.email as string, icon: 'i-heroicons-user-circle-16-solid' }] : []),
-    appLinks.value.index,
-    appLinks.value.list,
-    appLinks.value.calendar
-  ];
+  return [appLinks.value.index, appLinks.value.list, appLinks.value.calendar];
 });
 
 const logout = () => {
@@ -27,13 +22,31 @@ const logout = () => {
 
 <template>
   <USlideover v-model="modelValue">
-    <div class="p-4 flex-1">
+    <UCard
+        class="flex flex-col flex-1"
+        :ui="{ body: { base: 'flex-1' }, ring: '', divide: 'divide-y divide-gray-100 dark:divide-gray-800' }"
+    >
+      <template #header>
+        <div class="flex justify-between items-center">
+          <div class="flex items-center gap-4">
+            <UIcon class="w-5 h-5 text-primary" name="i-heroicons-user-circle-16-solid" />
+            <p class="text-base/5 font-semibold text-gray-900 dark:text-white">{{ user?.email }}</p>
+          </div>
+          <UButton
+              class="-my-1"
+              color="gray"
+              icon="i-heroicons-x-mark-20-solid"
+              variant="ghost"
+              @click="modelValue = false"
+          />
+        </div>
+      </template>
       <UVerticalNavigation :links="links">
         <template #default="{ link }">
           <span class="group-hover:text-primary relative">{{ link.label }}</span>
         </template>
       </UVerticalNavigation>
       <UButton v-if="authenticated" block class="mt-4" label="Выход" @click="logout" />
-    </div>
+    </UCard>
   </USlideover>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -7,20 +7,22 @@ const links = computed(() => {
 </script>
 
 <template>
-  <UContainer class="flex flex-wrap justify-center gap-4 py-4">
-    <UCard
-        v-for="(link, linkIndex) in links"
-        class="relative z-0 w-full max-w-xs pointer-events-none has-[:hover]:ring-2 has-[:hover]:ring-primary-500 dark:has-[:hover]:ring-primary-400 has-[:hover]:bg-gray-100/50 dark:has-[:hover]:bg-gray-800/50"
-        :key="`link-${linkIndex}`"
-        :ui="{ body: { base: 'flex gap-6 items-center' } }"
-    >
-      <UIcon class="w-10 h-10 text-primary" :name="link.icon" />
-      <ULink
-          class="text-base font-semibold text-gray-900 dark:text-white truncate focus-visible:outline-none pointer-events-auto before:absolute before:inset-0 before:-z-[1]"
-          :to="link.to"
+  <ClientOnly>
+    <UContainer class="flex flex-wrap justify-center gap-4 py-4">
+      <UCard
+          v-for="(link, linkIndex) in links"
+          class="relative z-0 w-full max-w-xs pointer-events-none has-[:hover]:ring-2 has-[:hover]:ring-primary-500 dark:has-[:hover]:ring-primary-400 has-[:hover]:bg-gray-100/50 dark:has-[:hover]:bg-gray-800/50"
+          :key="`link-${linkIndex}`"
+          :ui="{ body: { base: 'flex gap-6 items-center' } }"
       >
-        {{ link.label }}
-      </ULink>
-    </UCard>
-  </UContainer>
+        <UIcon class="w-10 h-10 text-primary" :name="link.icon" />
+        <ULink
+            class="text-base font-semibold text-gray-900 dark:text-white truncate focus-visible:outline-none pointer-events-auto before:absolute before:inset-0 before:-z-[1]"
+            :to="link.to"
+        >
+          {{ link.label }}
+        </ULink>
+      </UCard>
+    </UContainer>
+  </ClientOnly>
 </template>
