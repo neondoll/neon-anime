cff88b18daa986ae3befb1b45ad379c90465e833
neondoll <bonni_1998@mail.ru>
2024-06-08 13:48:06 +0300

    Deploy to GitHub Pages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,7 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ['@nuxt/ui', "@nuxtjs/supabase", '@pinia/nuxt', '@samk-dev/nuxt-vcalendar'],
+  nitro: { prerender: { failOnError: false } },
   pinia: { storesDirs: ['./stores/**'] },
   supabase: { url: process.env.SUPABASE_URL, key: process.env.SUPABASE_KEY }
 })
